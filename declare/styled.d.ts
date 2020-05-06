9acc3d2adeda009e512005179e82746247e75274
Pablo Berganza <pberganza@applaudostudios.com>
2020-05-06 13:47:28 -0600

    :sparkles: Add styled-components global theme type declaration

---
+++ declare/styled.d.ts
@@ -0,0 +1,47 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    desktopSmall: string;
+    tablet: string;
+    phone: string;
+    phoneSmall: string;
+    gridWidth: string;
+    boxShadow: string;
+    fonts: {
+      sizes: {
+        biggest: string;
+        bigger: string;
+        big: string;
+        regular: string;
+        small: string;
+        smaller: string;
+        smallest: string;
+      };
+    };
+    colors: {
+      white: string;
+      mortgageRed: string;
+      primaryDark: string;
+      darkGray: string;
+      gray: string;
+      mediumGray: string;
+      lightGray: string;
+      lightestGray: string;
+      lightBlue: string;
+      dark: colorDark;
+      darkBlue: string;
+      mainHeadingColor: string;
+      paragraph: {
+        dark: string;
+        lightGray: string;
+        darkGray: string;
+      };
+      spanColor: string;
+      primary: string;
+      grayLight: string;
+      grayBorders: string;
+      shadowColor: string;
+    };
+  }
+}
