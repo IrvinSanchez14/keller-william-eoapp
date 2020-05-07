9aa787f42a35314848c43cb8e556f82ea5bc1f61
Fernando Juarez <fjuarez@applaudostudios.com>
2020-05-07 13:25:57 -0600

    :art: set correct type

--- src/store/models/AppState.ts
+++ src/store/models/AppState.ts
@@ -59,7 +59,7 @@ export interface MetaDataProps {
 export interface AppInitalProps {
   email: string;
   data: DataInitalProps;
-  providers: Object;
+  providers: Record<string, any>;
   metadata: MetaDataProps;
 }
 
