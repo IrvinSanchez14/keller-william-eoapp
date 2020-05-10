c036674bada802f682fdf29c8e541f7558659551
Fernando Juarez <fjuarez@applaudostudios.com>
2020-05-10 01:23:35 -0600

    :bug: Firm Information - Email Field
    fix: add email validation

--- src/helpers/validations/index.ts
+++ src/helpers/validations/index.ts
@@ -12,7 +12,9 @@ export const fullEmailValidateSchema = Yup.object().shape({
   suite: Yup.number().required('Field is required'),
   phoneNumber: Yup.number().required('Field is required'),
   faxNumber: Yup.number().required('Field is required'),
-  emailAddress: Yup.string().required('Field is required'),
+  emailAddress: Yup.string()
+    .email('Email Address must be a valid email')
+    .required('Field is required'),
 });
 
 export const dateBrokerValidateSchema = Yup.object().shape({
