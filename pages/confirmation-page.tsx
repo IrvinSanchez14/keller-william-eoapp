656816f356bc2944b2e2f9ed4817eb59ebc70a0e
Fernando Juarez <fjuarez@applaudostudios.com>
2020-05-21 20:30:45 -0600

    :fire: refactor!: remove <Head /> tag

--- pages/confirmation-page.tsx
+++ pages/confirmation-page.tsx
@@ -14,29 +14,6 @@ const MyApp: React.FC = () => {
   if (state === 'error' || (data && !data.confirmationNumber)) return <Error statusCode={404} />;
   return (
     <div>
-      <Head>
-        <title>Keller Covered</title>
-        <link rel="icon" href="/favicon.ico" />
-        <script src="https://kit.fontawesome.com/0f6ae094e0.js" crossOrigin="anonymous" />
-        <link
-          href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-shims.min.css"
-          media="all"
-          rel="stylesheet"
-          id="font-awesome-5-kit-css"
-        />
-        <link
-          href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-font-face.min.css"
-          media="all"
-          rel="stylesheet"
-          id="font-awesome-5-kit-css"
-        />
-        <link
-          href="https://kit-pro.fontawesome.com/releases/latest/css/pro.min.css"
-          media="all"
-          rel="stylesheet"
-          id="font-awesome-5-kit-css"
-        />
-      </Head>
       {data && (
         <ConfirmationPage
           confirmationNumber={data.confirmationNumber}
