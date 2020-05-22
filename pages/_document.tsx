0c2cc45791abf0fdfb6f91b15bcd4e9239f5dba8
Fernando Juarez <fjuarez@applaudostudios.com>
2020-05-21 20:28:15 -0600

    feat: add favicon feat: place <Head /> only in one place

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
 
@@ -30,5 +30,64 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/img/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/img/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/img/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/img/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/static/img/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <script src="https://kit.fontawesome.com/0f6ae094e0.js" crossOrigin="anonymous" defer />
+
+          <link
+            href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-shims.min.css"
+            media="all"
+            rel="stylesheet"
+            id="font-awesome-5-kit-css"
+          />
+          <link
+            href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-font-face.min.css"
+            media="all"
+            rel="stylesheet"
+            id="font-awesome-5-kit-css"
+          />
+          <link
+            href="https://kit-pro.fontawesome.com/releases/latest/css/pro.min.css"
+            media="all"
+            rel="stylesheet"
+            id="font-awesome-5-kit-css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
