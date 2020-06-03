c1f97911f1b27636f92b4ece211a9426ac9904bd
Israel Alfaro <ihernandez@applaudostudios.com>
2020-06-03 12:49:01 -0600

    :bug: Added P component

---
+++ src/components/P/index.tsx
@@ -0,0 +1,51 @@
+import styled, { css } from 'styled-components';
+
+interface IProps {
+  isSmall?: boolean;
+  props?: any;
+}
+
+interface StyledParagraphProps {
+  margin?: string;
+  mobileMargin?: string;
+  isSmall?: boolean;
+}
+
+const StyledParagraph = styled.p<StyledParagraphProps>`
+  color: ${({ theme: { colors }, color }) =>
+    color === 'white' ? colors.white : colors.paragraph.dark};
+  font-size: 22px;
+  line-height: 28px;
+  ${({ margin }) => margin && `margin: ${margin};`};
+  ${({ theme, mobileMargin }) => theme.phone`
+    font-size: 18px;
+    line-height: 24px;
+    ${mobileMargin && `margin: ${mobileMargin}`};
+  `};
+  ${({ isSmall, theme, color }) =>
+    isSmall &&
+    css`
+      font-size: 18px;
+      line-height: 26px;
+      color: ${(color === 'dark' && theme.colors.paragraph.darkGray) ||
+      (color === 'light' && theme.colors.paragraph.lightGray) ||
+      theme.colors.white};
+      ${theme.phone`
+        font-size: 16px;
+        line-height: 20px;
+      `};
+    `};
+`;
+
+const defaultProps: IProps = {
+  isSmall: false,
+};
+
+function P(Props: IProps) {
+  const { isSmall, ...props } = Props;
+  return <StyledParagraph isSmall={isSmall} {...props} />;
+}
+
+P.defaultProps = defaultProps;
+
+export default P;
