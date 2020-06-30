3acdbb07d1854fdbddd372dd5453f212dcca0089
Israel Alfaro <ihernandez@applaudostudios.com>
2020-06-30 13:40:41 -0600

    fix: resolved travis build error

--- src/containers/TreeEO/FirmInformation/form.tsx
+++ src/containers/TreeEO/FirmInformation/form.tsx
@@ -7,27 +7,28 @@ export const FormFirmInformation = (
   formikProps: FormikProps<any>,
   showMarketCenterNumber = true,
 ) => {
-  const fields = showMarketCenterNumber
-    ? getFullNameFields()
-    : getFullNameFields().filter((field) => field.name !== 'kwMarketCenterNumber');
-
-  return fields?.map(({ name, type, customWidth, label, numberMask, setNumberMask }) => (
-    <FielControlForm
-      data-test-id={name}
-      id={name}
-      key={name}
-      name={name}
-      type={type}
-      setFieldTouched={formikProps.setFieldTouched}
-      label={label}
-      fullWidth
-      shouldValidateOnMount
-      errors={formikProps.errors}
-      touched={formikProps.touched}
-      renderFastField
-      customWidth={customWidth}
-      numberMask={numberMask}
-      setNumberMask={setNumberMask}
-    />
-  ));
+  return getFullNameFields().map(
+    ({ name, type, customWidth, label, numberMask, setNumberMask }) => {
+      if (name === 'kwMarketCenterNumber' && !showMarketCenterNumber) return null;
+      return (
+        <FielControlForm
+          data-test-id={name}
+          id={name}
+          key={name}
+          name={name}
+          type={type}
+          setFieldTouched={formikProps.setFieldTouched}
+          label={label}
+          fullWidth
+          shouldValidateOnMount
+          errors={formikProps.errors}
+          touched={formikProps.touched}
+          renderFastField
+          customWidth={customWidth}
+          numberMask={numberMask}
+          setNumberMask={setNumberMask}
+        />
+      );
+    },
+  );
 };
