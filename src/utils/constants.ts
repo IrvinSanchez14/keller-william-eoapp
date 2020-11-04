90d9b32271cbb6efcf4ff564c200e9094126783e
Irvin Sanchez <irvnsanchez@gmail.com>
2020-11-04 10:22:42 -0600

    fix: text branch

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 const API_URL_STATING = 'https://eo-dev-api.kellercovered.io/api/v1';
+
 const API_URL_PRODUCTION = 'https://eo-api.kellercovered.com/api/v1';
 const NEW_API_URL_STAGING = 'https://api2-dev.kellercovered.io/kc-svc-eo-application/api/v1';
 const NEW_API_URL_PRODUCTION = 'https://api2-dev.kellercovered.io/kc-svc-eo-application/api/v1';
